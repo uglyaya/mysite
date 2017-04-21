62c1eaa5f7dd3915c2c17ea0885e5c530ef05215
chenjie.zhou <chenjie.zhou@dianjoy.com>
2017-04-21 19:44:16 +0800

    增加xplugin

---
+++ hooked_server/xpluging.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+'''
+Created on 2017年4月21日
+
+@author: zhouc
+'''
+# class SCPCardOverviewPlugin(BaseAdminPlugin):
+#     pool_overview=False#默认不加载此插件
+#     def init_request(self, *args, **kwargs):
+#         return bool(self.pool_overview)
+# xadmin.site.register_plugin(SCPCardOverviewPlugin, ListAdminView)
+
+if __name__ == '__main__':
+    pass
